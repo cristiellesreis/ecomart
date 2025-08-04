6fc2a1cbb4d147fb84ffc8cd55fb48548183d98c
cristiellesreis <cristielle.santos94@gmail.com>
2025-08-04 15:56:02 -0300

    feat: integrando contagem de tokens

--- src/main/java/br/com/alura/ecomart/controller/CategorizadorDeProdutosController.java
+++ src/main/java/br/com/alura/ecomart/controller/CategorizadorDeProdutosController.java
@@ -1,5 +1,8 @@
 package br.com.alura.ecomart.controller;
 
+import com.knuddels.jtokkit.Encodings;
+import com.knuddels.jtokkit.api.EncodingType;
+import com.knuddels.jtokkit.api.ModelType;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.ai.chat.prompt.DefaultChatOptionsBuilder;
@@ -39,6 +42,11 @@ public class CategorizadorDeProdutosController {
                 Resposta: Esportes
                 """;
 
+        var tokens = contarTokens(system, produto);
+        System.out.println("QTD de tokens: " + tokens);
+
+        //logica de escolha de modelo baseada no numero de tokens
+
         return this.chatClient.prompt()
                 .system(system)
                 .user(produto)
@@ -47,5 +55,11 @@ public class CategorizadorDeProdutosController {
                         .build())
                 .call()
                 .content();
+    }
+
+    private int contarTokens(String system, String user){
+        var registry = Encodings.newDefaultEncodingRegistry();
+        var enc = registry.getEncodingForModel(ModelType.GPT_4O_MINI);
+        return enc.countTokens(system + user);
     }
 }
